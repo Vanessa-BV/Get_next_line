91952ea8351aec2cedd2ec0d546e4bc4015306dd
Vanessa <vanessa@martins-air.kpn>
2023-11-06 18:58:24 +0100

    added all files

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+/*** allocate memory for buffer (malloc) ***/
+
+/*** read file (read) ***/
+// implement logic to not read a binary file
+
+/*** free memory (free) ***/
+}
+
+int	main(void)
+{
+	int	file_descriptor;
+
+	file_descriptor = open("file", O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		printf("File could not be opened.\n");
+		return -1;
+	}
+	close(file_descriptor);
+	return (0);
+}
