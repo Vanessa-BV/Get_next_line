c804fbf4d83e47454b9ca0f231a590a441a0864f
Vanessa <116423200+Vanessa-BV@users.noreply.github.com>
2023-12-01 11:28:10 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,30 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
+//for read(), open(), close()
+# include <unistd.h>
+# include <fcntl.h>
+// for printf
+# include <stdio.h>
+// for malloc() and free()
+# include <stdlib.h>
+// for boolean
+# include <stdbool.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
-#endif
-
-typedef struct Node
-{
-	char		*content;
-	struct Node	*next;
-}	t_list;
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 char	*get_next_line(int fd);
-int		found_newline(t_list *stash);
-t_list	*ft_lst_get_last(t_list *stash);
-void	generate_line(char **line, t_list *stash);
-void	free_stash(t_list *stash);
-int		ft_strlen(const char *str);
-void	ft_read_and_stash(int fd, t_list **stash);
-void	add_to_stash(t_list **stash, char *buff, int no_char_read);
-void	extract_line(t_list *stash, char **line);
-void	clean_stash(t_list **stash);
-char	*get_next_line(int fd);
+char	*read_and_stash(int fd, char *buffer, char *str_stash);
+char	*stash_to_line(char *str_stash);
+void	update_stash(char *str_stash);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
+size_t	newline_index(const char *s);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	free_all(char **str);
 #endif
