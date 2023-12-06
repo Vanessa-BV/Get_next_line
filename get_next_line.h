feed4ccf6c9479dc4701033b047d60b832aeda05
Vanessa <116423200+Vanessa-BV@users.noreply.github.com>
2023-12-06 11:33:06 +0100

    Final get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,27 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-//for read(), open(), close()
 # include <unistd.h>
 # include <fcntl.h>
-// for printf
 # include <stdio.h>
-// for malloc() and free()
 # include <stdlib.h>
-// for boolean
-# include <stdbool.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
 
+typedef struct s_data
+{
+	char	*line;
+	int		nl_index;
+}	t_data;
+
 char	*get_next_line(int fd);
-char	*read_and_stash(int fd, char *buffer, char *str_stash);
-char	*stash_to_line(char *str_stash);
-void	update_stash(char *str_stash);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *str);
-size_t	newline_index(const char *s);
-size_t	ft_strlen(const char *s);
+int		buffer_to_line(t_data *data, char *buffer);
+int		string_char(const char *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int		trim_line(t_data *data);
+int		read_and_stash(t_data *data, int fd, char *buffer);
 char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	free_all(char **str);
 #endif
