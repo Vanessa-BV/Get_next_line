c4cbc4759098e8c39f8a0ecc789e01072c76d4b9
Vanessa <116423200+Vanessa-BV@users.noreply.github.com>
2023-12-06 11:32:46 +0100

    final get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,125 +1,81 @@
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	trim_line(t_data *data)
 {
-	static char	*stash;
-	char		*line;
-	char		*buffer;
+	char	*new_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (stash != NULL && ft_strchr(stash, '\n') != NULL)
-		line = stash_to_line(stash);
-	else
-	{
-		buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-		if (buffer == NULL)
-			return (free_all(&stash), NULL);
-		stash = read_and_stash(fd, buffer, stash);
-		if (stash == NULL)
-			return (free_all(&buffer), NULL);
-		free_all(&buffer);
-		line = stash_to_line(stash);
-	}
-	if (line == NULL)
-		return (free_all(&stash), NULL);
-	update_stash(stash);
-	if (*line == '\0')
-		return(free_all(&line), NULL);
-	return (line);
+	data->nl_index = string_char(data->line, '\n');
+	if (data->nl_index == -1)
+		return (0);
+	new_line = (char *)malloc((data->nl_index + 2) * sizeof(char));
+	if (new_line == NULL)
+		return (-1);
+	ft_strlcpy(new_line, data->line, data->nl_index + 2);
+	free(data->line);
+	data->line = new_line;
+	return (0);
 }
 
-char	*read_and_stash(int fd, char *buffer, char *str_stash)
+int	buffer_to_line(t_data *data, char *buffer)
+{
+	char	*str;
+
+	str = (char *)malloc((ft_strlen(buffer) + 1) * sizeof(char));
+	if (str == NULL)
+		return (-1);
+	data->line = str;
+	data->nl_index = string_char(buffer, '\n');
+	if (data->nl_index == -1)
+		ft_strlcpy(str, buffer, ft_strlen(buffer) + 1);
+	else
+		ft_strlcpy(str, buffer, data->nl_index + 2);
+	return (0);
+}
+
+int	read_and_stash(t_data *data, int fd, char *buffer)
 {
 	int		bytes_read;
-	char	*temp_stash;
+	char	*temp;
 
 	bytes_read = 1;
-	if (str_stash == NULL)
-		str_stash = ft_strdup("");
-	if (str_stash == NULL)
-		return (NULL);
-	while (bytes_read != 0)
+	while (bytes_read != 0 && string_char(buffer, '\n') == -1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return(free_all(&str_stash), NULL);
+			return (-1);
 		buffer[bytes_read] = '\0';
-		temp_stash = str_stash;
-		str_stash = ft_strjoin(temp_stash, buffer);
-		if (str_stash == NULL)
-			return (free_all(&temp_stash), NULL);
-		free_all(&temp_stash);
-		if (ft_strchr(str_stash, '\n') != NULL)
-			break ;
+		temp = ft_strjoin(data->line, buffer);
+		if (temp == NULL)
+			return (-1);
+		free(data->line);
+		data->line = temp;
 	}
-	return (str_stash);
+	return (0);
 }
 
-char	*stash_to_line(char *str_stash)
+char	*get_next_line(int fd)
 {
-	size_t	i;
-	char	*str_line;
+	t_data		data;
+	static char	buffer[BUFFER_SIZE + 1];
 
-	i = 0;
-	str_line = (char *)malloc((newline_index(str_stash) + 2) * sizeof(char));
-	if (str_line == NULL)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while ((str_stash[i]) && str_stash[i] != '\n')
+	if (buffer_to_line(&data, buffer) == -1)
+		return (NULL);
+	if (read_and_stash(&data, fd, buffer) == -1)
 	{
-		str_line[i] = str_stash[i];
-		if (str_line[0] == '\0')
-			return (free_all(&str_line), NULL);
-		i++;
+		buffer[0] = '\0';
+		return (free(data.line), NULL);
 	}
-	if (str_stash[i] == '\n')
-		str_line[i++] = '\n';
-	str_line[i] = '\0';
-	return (str_line);
-}
-
-void	update_stash(char *str_stash)
-{
-	// size_t	newline_index;
-	char	*source;
-	size_t	i;
-
-	i = 0;
-	source = ft_strchr(str_stash, '\n');
-	if (source == NULL)
-	{
-		free_all(&str_stash);
-		return ;
-	}
-	source++;
-	while (source[i] != '\0')
-	{
-		str_stash[i] = source[i];
-		i++;
-	}
-	str_stash[i] = '\0';
-	i++;
-	while (str_stash[i] != '\0')
-	{
-		str_stash[i] = '\0';
-		i++;
-	}
-	// newline_index = 0;
-	// while (str_stash[newline_index] != '\n')
-	// 	newline_index++;
-	
-	// ft_memmove(str_stash, str_stash + newline_index + 1,
-	// 	(ft_strlen(str_stash) - newline_index) * sizeof(char));
-	
-}
-
-void	free_all(char **str)
-{
-	if (str != NULL && *str != NULL)
-	{
-		free(*str);
-	}
-	*str = NULL;
+	if (trim_line(&data) == -1)
+		return (free(data.line), NULL);
+	data.nl_index = string_char(buffer, '\n');
+	if (data.nl_index != -1)
+		ft_strlcpy(buffer, &buffer[data.nl_index + 1], \
+		ft_strlen(&buffer[data.nl_index + 1]) + 1);
+	if (data.line[0] == '\0')
+		return (free(data.line), NULL);
+	return (data.line);
 }
 
 // int	main(void)
@@ -127,24 +83,26 @@ void	free_all(char **str)
 // 	int		fd;
 // 	char	*line;
 
-// 	fd = open("1char.txt", O_RDONLY);
+// 	fd = open("giant_line.txt", O_RDONLY);
 // 	if (fd < 0)
 // 	{
 // 		printf("Error ocurred while opening the file.\n");
 // 		return (1);
 // 	}
-// 	for (int i = 0; i < 5; i++)
+// 	// for (int i = 0; i < 10; i++)
+// 	// {
+// 	// 	line = get_next_line(fd);
+// 	// 	printf("%s", line);
+// 	// 	free (line);
+// 	// }
+// 	while (1)
 // 	{
 // 		line = get_next_line(fd);
 // 		printf("%s", line);
+// 		if (line == NULL)
+// 			break ;
 // 		free (line);
 // 	}
-// 	// while ((line = get_next_line(fd)) != NULL)
-// 	// {
-// 	// 	printf("%s", line);
-// 	// 	free_all(&line);
-// 	// }
 // 	close(fd);
 // 	return (0);
 // }
-
